639b66a24951f0a1cd3c52f61abddae299ee4919
Pritchad25 <pricts29@gmail.com>
2025-08-09 12:06:31 +0000

    create UserViewSet class.

--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
-from .serializers import RegisterSerializer
-
+from rest_framework import generics, viewsets
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    '''A User View'''
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 class RegisterView(generics.CreateAPIView):
 	'''A Registration View.'''
