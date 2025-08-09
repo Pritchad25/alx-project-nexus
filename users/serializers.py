5722392d23e5f8cfc038abf8a7aeb13145038844
Pritchad25 <pricts29@gmail.com>
2025-08-09 12:10:40 +0000

    adjusting UserSerializer class.

--- users/serializers.py
+++ users/serializers.py
@@ -1,24 +1,7 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from django.contrib.auth.password_validation import validate_password
+from .models import User
 
-User = get_user_model()
-
-class RegisterSerializer(serializers.ModelSerializer):
-	'''Register Serializer'''
-	password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-	password2 = serializers.CharField(write_only=True, required=True)
-
-	class Meta:
-		model = User
-		fields = ['username', 'email', 'password', 'password2']
-
-	def validate(self, attrs):
-		if attrs['password'] != attrs['password2']:
-			raise serializers.ValidationError({"password": "Passwords do not match."})
-		return attrs
-
-	def create(self, validated_data):
-		validated_data.pop('password2')
-		user = User.objects.create_user(**validated_data)
-		return user
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
