a28570a4ad13a5ce089f3f97969bf2d9bd98c924
Pritchad25 <pricts29@gmail.com>
2025-08-12 06:14:46 +0000

    scaffold media routing.

--- products/admin.py
+++ products/admin.py
@@ -5,3 +5,9 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'stock', 'is_active', 'created_at']
     search_fields = ['name', 'description']
+    readonly_fields = ['image_preview']
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" />', obj.image.url)
+        return "-"
