d306445308485df78be64e41e4121273b3e108d4
Pritchad25 <pricts29@gmail.com>
2025-08-12 11:51:04 +0000

    Update ViewSet for Filtering and Sorting.

--- products/views.py
+++ products/views.py
@@ -3,9 +3,21 @@ from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['category']
+    ordering_fields = ['price']
+    ordering = ['price']  # default ordering
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.request.query_params.get('category')
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
