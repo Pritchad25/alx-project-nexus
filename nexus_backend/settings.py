b9aebb426a24632db926de7e28cf6f5e8a96e64e
Pritchad25 <pricts29@gmail.com>
2025-08-12 11:44:10 +0000

    scaffolding new categories app.

--- nexus_backend/settings.py
+++ nexus_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'products',
+    'categories',
 ]
 
 MIDDLEWARE = [
@@ -75,6 +76,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 SIMPLE_JWT = {
