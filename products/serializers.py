9a6b776b9fb5dc4deeeaf0f1d4c8f240f11f5cb0
Pritchad25 <pricts29@gmail.com>
2025-08-12 11:49:36 +0000

    add category field to Product Model.

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Product
+from .models import Product, Category
 
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False)
-
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    
     class Meta:
         model = Product
         fields = '__all__'
